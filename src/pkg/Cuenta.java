a8935a1525c551e79e7ec1769142fa731a3d96c6
lanid <lanid@PCLaura>
2025-10-06 18:46:49 +0200

    Paso 2 cuenta. Solventar errores

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,49 @@
+package pkg;
+
+public class Cuenta {
+	String numero;
+	String titular;
+	Double saldo;
+	
+	public Cuenta(Double i) {
+		this.saldo = i;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public Double getSaldo() {
+		return 200.0;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void ingresar(double d) {
+		// TODO Auto-generated method stub
+		this.saldo=200.0;
+		
+	}
+
+	public void retirar(double i) {
+		// TODO Auto-generated method stub
+		this.saldo=-200.0;
+		
+	}
+	
+	
+}
--- src/test/testCuenta.java
+++ src/test/testCuenta.java
@@ -8,13 +8,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pkg.Cuenta;
+
 class testCuenta {
 	
-	pruvate static Cuenta cuenta;
+	private static Cuenta cuenta;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		cuenta = new Cuenta(0);
+		cuenta = new Cuenta(0.0);
 	}
 
 	@AfterAll
@@ -31,13 +33,13 @@ class testCuenta {
 
 	@Test
 	void testIngresar() {
-		cuenta.ingresar(200);
-		assertEquals(200, cuenta.getSaldo());
+		cuenta.ingresar(200.0);
+		assertEquals(200.0, cuenta.getSaldo());
 	}
 	
 	@Test
 	void testRetirar() {
-		cuenta.retirar(200);
+		cuenta.retirar(200.0);
 		assertEquals(-200, cuenta.getSaldo());
 	}
 
